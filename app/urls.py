d1dac34c09e88126b9b994e7b2e30e331d21b66e
Sudhir Singh <38358716+crepantherx@users.noreply.github.com>
2025-11-18 16:15:55 +0530

    Update urls.py

--- app/urls.py
+++ app/urls.py
@@ -4,6 +4,17 @@ from . import views
 handler404 = 'app.views.custom_404_view'
 handler500 = 'app.views.custom_500_view'
 
+from django.http import HttpResponse
+import os
+from django.conf import settings
+from django.urls import path
+
+def sitemap_view(request):
+    sitemap_path = os.path.join(settings.BASE_DIR, 'sitemap.xml')
+    with open(sitemap_path, 'r') as f:
+        sitemap_content = f.read()
+    return HttpResponse(sitemap_content, content_type='application/xml')
+
 urlpatterns = [
     path('', views.home, name='home'),
     path('services', views.services, name='services'),
@@ -18,6 +29,7 @@ urlpatterns = [
     path('logout/', views.logout_view, name='logout'),
     path('profile/', views.profile_view, name='profile'),
     path('pricing/', views.pricing, name='pricing'),
+    path('sitemap.xml', sitemap_view, name='sitemap'),
     # path('chart/', views.chart, name='chart'),
     # path('upload-csv/', views.upload_csv_view, name='upload_csv'),
 
